54e75be9d12f35c6bcfbf6fbcc84cdbbd7d21120
rjames187 <rory.james2021@gmail.com>
2023-09-09 18:48:52 -0500

    add command entry prefix for ux

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 )
 
@@ -9,8 +10,10 @@ func main() {
 
 	reader := bufio.NewScanner(os.Stdin)
 	runner := NewRunner()
+	fmt.Print("Pokedex > ")
 	for reader.Scan() {
 		input := parseInput(reader.Text())
 		runner.exeCommand(input[0])
+		fmt.Print("Pokedex > ")
 	}
 }
